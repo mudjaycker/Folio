2e9fa120ea8b1fbe09adc48b86285c0e45701000
mudjaycker <tombutoyi@gmail.com>
2024-04-12 01:11:56 +0200

    footer buttons

--- ui_ts/src/components/Hero.svelte
+++ ui_ts/src/components/Hero.svelte
@@ -3,13 +3,28 @@
     <p>Hi I am Maryimana</p>
   </section>
 
-  <section id="footer"></section>
+  <section id="footer">
+    {#each ["About me", "Contact me"] as text}
+      <div class="footer-item">
+        <a href="/">{text}</a>
+      </div>
+    {/each}
+  </section>
 </section>
 
 <style lang="scss">
   $_1st-bg: #8b9aaf;
   $_2nd-bg: #3f44e3;
   $_3rd-bg: #292c2c;
+
+  @mixin px($x: 1%) {
+    padding-left: $x;
+    padding-right: $x;
+  }
+  @mixin mx($x: 1%) {
+    margin-left: $x;
+    margin-right: $x;
+  }
 
   #container {
     background-color: $_1st-bg;
@@ -33,6 +48,27 @@
     position: fixed;
     bottom: 0;
     display: flex;
-    justify-content: space-between;
+    justify-content: center;
+    align-items: center;
+
+    :nth-child(2) {
+      margin-left: 20%;
+    }
+  }
+  .footer-item {
+    width: 20%;
+    background-color: #0d5a5a;
+    height: 50%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 5px;
+    &:hover{
+      background-color: $_2nd-bg;
+    }
+    a {
+      text-decoration: none;
+      color: white;
+    }
   }
 </style>
