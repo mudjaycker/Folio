8ff75938fb9f9687c35252bd50e70370e04f071a
mudjaycker <tombutoyi@gmail.com>
2024-04-12 00:17:34 +0200

    hero

--- ui_ts/src/components/Hero.svelte
+++ ui_ts/src/components/Hero.svelte
@@ -1,1 +1,20 @@
-<h1>test</h1>
+<section id="container">
+  <section id="header"></section>
+</section>
+
+<style lang="scss">
+  $_1st-bg: #9f9fa5;
+  $_2nd-bg: #3f44e3;
+
+  #container {
+    background-color: $_1st-bg;
+    width: 100vw;
+    height: 100vh;
+  }
+  #header {
+    background-color: $_2nd-bg;
+    width: 100%;
+    height: 20%;
+    position: fixed;
+  }
+</style>
