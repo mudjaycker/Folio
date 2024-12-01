354e5db59997b7c5f6f4ed3a5c933299df3bc0a5
mudjaycker <tombutoyi@gmail.com>
2024-12-01 16:19:00 +0200

    utils.loop function

--- svelte_spa/src/utils.ts
+++ svelte_spa/src/utils.ts
@@ -1,3 +1,6 @@
+export const list = Array.from;
+export const print_ = console.log;
+
 export function* range(
   begin: number,
   end: number | null = null,
@@ -9,5 +12,18 @@ export function* range(
 
   for (let i = begin; step < 0 ? i > end : i < end; i += step) {
     yield i;
+  }
+}
+
+export function* loop<T>(iterable: Iterable<T>): Generator<T> {
+  let index = 0;
+  let array = list(iterable);
+  let last_index = array.length - 1;
+  while (true) {
+    if (index == last_index) {
+      index = 0;
+    }
+    yield array[index];
+    index++;
   }
 }
