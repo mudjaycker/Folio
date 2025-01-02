6ce74147aaa4f44c38ae5abd14ac5219456755af
mudjaycker <tombutoyi@gmail.com>
2025-01-02 23:19:09 +0200

    back button

--- svelte_spa/src/App.svelte
+++ svelte_spa/src/App.svelte
@@ -3,11 +3,26 @@
   import { current_page } from "./store";
 
   $: pageName = $current_page.name;
+  function goBack() {
+    history.back();
+  }
 </script>
 
 <svelte:head>
-  <title>{pageName}</title>
+  <title>{pageName} | Maryimana</title>
 </svelte:head>
 <main>
+  {#if pageName != "Welcome"}
+    <button on:click={goBack} class="back-btn">Back</button>
+  {/if}
   <svelte:component this={$current_page.component} />
 </main>
+
+<style lang="scss">
+    .back-btn {
+      margin-top: 50px;
+      margin-left: 10px;
+      height: 50px;
+      width: 150px;
+    }
+</style>
