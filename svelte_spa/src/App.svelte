4aa76ac230a71d03799ff92cb68ac8a8b43b979f
mudjaycker <tombutoyi@gmail.com>
2025-10-20 23:54:34 +0200

    info links

--- svelte_spa/src/App.svelte
+++ svelte_spa/src/App.svelte
@@ -1,28 +1,84 @@
 <script lang="ts">
-  import "./router";
-  import { current_page } from "./store";
+    import "./router";
+    import { current_page } from "./store";
 
-  $: pageName = $current_page.name;
-  function goBack() {
-    history.back();
-  }
+    $: pageName = $current_page.name;
+
+    function goBack() {
+        history.back();
+    }
+    
+    const telegramlink = "www.telegram.me/mudjaygram",
+        githublink = "www.github.com/maryimana",
+        whatsapplink = "web.whatsapp.com/0025772037069",
+        gmaillink = "maryimana983@gmail.com",
+        linkedinlink = "www.linkedin.com/in/maryimana-butoyi-bba2aa21b/";
+
+    const openLink = (link: string) => {
+        const url = "https://" + link;
+        window.open(url, "_blank");
+    };
+
+    let infos = [
+        { name: "Github", link: githublink, detail: "maryimana" },
+        { name: "Telegram", link: telegramlink, detail: "mudjaygram" },
+        { name: "Whatsapp", link: whatsapplink, detail: "00257772037069" },
+        { name: "Gmail", link: gmaillink, detail: "maryimana983@gmail.com" },
+        { name: "Linkedin", link: linkedinlink, detail: "maryimana" },
+    ];
 </script>
 
 <svelte:head>
-  <title>{pageName} | Maryimana</title>
+    <title>{pageName} by Maryimana</title>
 </svelte:head>
 <main>
-  {#if pageName != "Welcome"}
-    <button on:click={goBack} class="back-btn">👈🏿</button>
-  {/if}
-  <svelte:component this={$current_page.component as any} />
+    {#if pageName != "Welcome"}
+        <button on:click={goBack} class="back-btn">👈🏿</button>
+    {/if}
+    <svelte:component this={$current_page.component as any} />
+    {#if pageName == "Presentation"}
+        <div class="infos-container">
+            <div class="infos">
+                {#each infos as info}
+                    <p>
+                        {info.name}:
+                        <!-- svelte-ignore a11y_click_events_have_key_events -->
+                        <!-- svelte-ignore a11y_no_static_element_interactions -->
+                        <!-- svelte-ignore a11y_missing_attribute -->
+                        <a on:click={() => openLink(info.link)}>{info.detail}</a
+                        >
+                    </p>
+                {/each}
+            </div>
+        </div>
+    {/if}
 </main>
 
 <style lang="scss">
     .back-btn {
-      margin-top: 20px;
-      margin-left: 10px;
-      font-size: xx-large;
-      border-radius: 10px;
+        margin-top: 20px;
+        margin-left: 10px;
+        font-size: xx-large;
+        border-radius: 10px;
     }
+    .infos-container {
+        display: flex;
+        justify-content: center;
+        width: 100%;
+        .infos {
+            color: white;
+            background-color: #2c2929;
+            // position: absolute;
+            margin-bottom: 50px;
+            padding: 10px;
+            width: 50vw;
+            justify-content: center;
+            a {
+                color: rgb(186, 189, 8);
+                cursor: pointer;
+            }
+        }
+    }
+    // @media only screen and (max-width: 600px) {
+    // }
 </style>
--- svelte_spa/src/utils.ts
+++ svelte_spa/src/utils.ts
@@ -1,42 +1,52 @@
 export const list = Array.from;
 export const print_ = console.log;
 export const int = (num: string | number) => {
-  if (!num) {
-    return 0;
-  } else {
-    return parseInt(`${Number(num)}`);
-  }
+    if (!num) {
+        return 0;
+    } else {
+        return parseInt(`${Number(num)}`);
+    }
 };
 export function* range(
-  begin: number,
-  end: number | null = null,
-  step: number = 1
+    begin: number,
+    end: number | null = null,
+    step: number = 1
 ): Generator<number> {
-  if (end == null) {
-    [end, begin] = [begin, 0];
-  }
+    if (end == null) {
+        [end, begin] = [begin, 0];
+    }
 
-  for (let i = begin; step < 0 ? i > end : i < end; i += step) {
-    yield i;
-  }
+    for (let i = begin; step < 0 ? i > end : i < end; i += step) {
+        yield i;
+    }
 }
 
 export function* loop<T>(iterable: Iterable<T>): Generator<T> {
-  let index = 0;
-  let array = list(iterable);
-  let last_index = array.length - 1;
-  while (true) {
-    if (index == last_index) {
-      index = 0;
+    let index = 0;
+    let array = list(iterable);
+    let last_index = array.length - 1;
+    while (true) {
+        if (index == last_index) {
+            index = 0;
+        }
+        yield array[index];
+        index++;
     }
-    yield array[index];
-    index++;
-  }
 }
 
 export const filterNumIn = (num: string | number, chars: string[]) => {
-  return String(num)
-    .split("")
-    .filter((x) => chars.includes(x))
-    .join("");
+    return String(num)
+        .split("")
+        .filter((x) => chars.includes(x))
+        .join("");
 };
+
+export function isNumeric(value: string) {
+    let numbers = list(range(0, 10)).map((i) => String(i)); // list from 0 to 9
+    for (let i of value) {
+        if (!numbers.includes(i)) {
+            return false;
+        }
+    }
+    return true;
+}
